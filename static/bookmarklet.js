9997ee396ff5278c1dc49e820bd56a292e0edddb
Peter Bengtsson <mail@peterbe.com>
2011-09-07 05:58:46 +0100

    improved bookmarklet

--- static/bookmarklet.js
+++ static/bookmarklet.js
@@ -7,13 +7,15 @@ var Lookup = (function() {
   var BASE_URL = 'http://toocoolfor.me';
   //var BASE_URL = 'http://toocool';
   var your_screen_name;
+  var _default_interval = 3, _interval = _default_interval;
 
   var screennames = [];
+  var known = {};
   function findScreenNames() {
     var new_screennames = [];
     $('a.tweet-screen-name').each(function(i, e) {
       var v = $.trim($(this).text());
-      if (v && $.inArray(e, screennames) == -1) {
+      if (v && $.inArray(v, screennames) == -1) {
         screennames.push(v);
         new_screennames.push(v);
       }
@@ -30,16 +32,22 @@ var Lookup = (function() {
          var screen_name, tag, prefix;
 
          $('div.tweet-content').each(function() {
-           screen_name = $('a.tweet-screen-name', this).text();
-
-           // or if it's you
-           if (screen_name == your_screen_name)
-             return;
            // only display results once
            if ($('a.followsyou,a.followsyounot', this).size())
              return;
 
-           if (response[screen_name]) {
+           screen_name = $('a.tweet-screen-name', this).text();
+           // or if it's you
+           if (screen_name == your_screen_name)
+             return;
+
+           var result = known[screen_name];
+           if (result === undefined) {
+             result = response[screen_name];
+             known[screen_name] = result;
+           }
+
+           if (result) {
              tag = $('<a>', {text: 'follows me'})
                .addClass('followsyou')
                  .css('color', 'green');
@@ -58,10 +66,24 @@ var Lookup = (function() {
                          .appendTo($('span.tweet-user-name', this));
 
          });
+
      },
      search: function() {
-       var names = findScreenNames();
+       setTimeout(function() {
+         Lookup.search();
+       }, _interval * 1000);
+
        your_screen_name = $.trim($('#screen-name').text());
+       var names = findScreenNames();
+       names = $.grep(names, function(e) {
+         return known[e] === undefined && e != your_screen_name;
+       });
+
+       if (!names.length) {
+         _interval++;  // nothing has changed
+         return;
+       }
+       _interval = _default_interval;
        var data = {
          you: your_screen_name,
          usernames: names.join(',')
